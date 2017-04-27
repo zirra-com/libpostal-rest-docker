cf1935f50633aeba094b73bfefc2ed57fdc020d3
Guiqiang Zhang <skyhit@gmail.com>
2017-04-27 15:59:58 +0800

    circleci configuration

---
+++ .deployment/circle/dockerhub_push.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Any subsequent command which fails will cause the shell script to exit immediately
+set -e
+
+DOCKER_REPOSITORY=$1
+UNIQUEID=$2
+
+
+# Login to docker hub
+docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASS
+    
+# Build the docker image
+echo Build the docker image $DOCKER_REPOSITORY:$UNIQUEID
+
+docker build -t $DOCKER_REPOSITORY:$UNIQUEID .
+
+# Deploy image to Docker Hub
+echo Pushing docker image $DOCKER_REPOSITORY:$UNIQUEID
+docker push $DOCKER_REPOSITORY:$UNIQUEID
+
---
+++ .deployment/circle/eb_deploy.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+set -e
+
+DOCKER_REPOSITORY=$1
+UNIQUEID=$2
+EB_ENVIRONMENT=$3
+
+if [ -z "$EB_REGION" ]; then
+  EB_REGION=us-east-1
+fi
+
+EB_BUCKET=elasticbeanstalk-us-east-1-819318868604
+if [ -z "$EB_APPLICATION" ]; then
+  EB_APPLICATION=libpostal-rest
+fi
+
+if [ -z "$EB_REGION" ]; then
+  EB_REGION=us-east-1
+fi
+
+
+DOCKERRUN_TEMPLATE=./.deployment/circle/Dockerrun.aws.json.template
+EB_VERSION_PATH=./.deployment/eb_version
+ZIP_FILE=$UNIQUEID-version.zip
+ZIP_PATH=$EB_VERSION_PATH/$ZIP_FILE
+ZIP_S3_PATH=CircleCI_versions/$EB_APPLICATION/$ZIP_FILE
+DOCKERRUN_FILE=$EB_VERSION_PATH/Dockerrun.aws.json
+
+# Create new Elastic Beanstalk version
+echo Creating $DOCKERRUN_FILE
+sed -e "s/<TAG>/$UNIQUEID/;s/<DOCKER_REPOSITORY>/$(echo $DOCKER_REPOSITORY | sed -e 's/\//\\\//g')/" < $DOCKERRUN_TEMPLATE > $DOCKERRUN_FILE
+
+# Create zip
+echo Creating $ZIP_PATH
+pushd $EB_VERSION_PATH
+zip -r $ZIP_FILE .
+popd
+
+echo Uploading $ZIP_PATH to s3://$EB_BUCKET/$ZIP_S3_PATH
+aws s3 cp $ZIP_PATH s3://$EB_BUCKET/$ZIP_S3_PATH
+
+echo Creating version $UNIQUEID
+aws elasticbeanstalk --region $EB_REGION create-application-version \
+    --application-name $EB_APPLICATION \
+    --version-label $UNIQUEID \
+    --source-bundle S3Bucket=$EB_BUCKET,S3Key=$ZIP_S3_PATH
+
+
+# Update Elastic Beanstalk environment to new version
+echo Deploying $UNIQUEID on $EB_ENVIRONMENT
+aws elasticbeanstalk --debug --region $EB_REGION update-environment --environment-name $EB_ENVIRONMENT --version-label $UNIQUEID
