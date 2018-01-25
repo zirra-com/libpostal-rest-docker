98c8885e5afa67d4ec13f836348211f28b0544f0
Guiqiang Zhang <skyhit@gmail.com>
2018-01-25 11:03:31 +0800

    build first

--- dockerhub_push.sh
+++ dockerhub_push.sh
@@ -5,17 +5,14 @@ set -e
 
 DOCKER_REPOSITORY=$1
 UNIQUEID=$2
-
-
-# Login to docker hub
-docker login -u $DOCKER_USER -p $DOCKER_PASS
     
 # Build the docker image
 echo Build the docker image $DOCKER_REPOSITORY:$UNIQUEID
-
 docker build -t $DOCKER_REPOSITORY:$UNIQUEID .
 
 # Deploy image to Docker Hub
 echo Pushing docker image $DOCKER_REPOSITORY:$UNIQUEID
+# Login to docker hub
+docker login -u $DOCKER_USER -p $DOCKER_PASS
 docker push $DOCKER_REPOSITORY:$UNIQUEID
 
